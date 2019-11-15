e472754859ccc502c06e1b36007a86962a0a192e
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-15 20:34:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
  if array.length == 1
-  puts array.join
+  return array.join
  elsif array.length > 1
   x = array[0..array.length - 2].join(", ") 
   x+= array.last.join(" and ")
