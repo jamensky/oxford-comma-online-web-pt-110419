6bf5d5c747c33c06c57cfb8604bf63af6acd1bfc
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-15 20:08:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+ if array.length == 1
+  puts array.join
+ elsif array.length > 1
+  x = array[0..array.length - 2].join(", ") 
+  x+= array.last.join(" and ")
+ end
+return x
 end
+
+
