24c35500ac406fc8699f4037288b6f77f9410a3f
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-15 20:51:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
  if array.length == 1
   return array.join
+ elsif array.lenght == 2 
+  return array.join(", ")
+ 
  elsif array.length >= 2
   x = array[0..array.length - 2].join(", ") 
   x+= array.last.prepend(", and ")
